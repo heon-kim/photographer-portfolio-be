20137588a65e9837e49e45c7752ea8ef31d56c5a
heon-kim <vndtka123@gmail.com>
2026-02-21 00:11:08 +0900

    [s3] 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { S3Service } from './s3/s3.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, S3Service],
 })
 export class AppModule {}
